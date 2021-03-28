1d1e257375f15abbdc5ff95f6af29375c535fa62
gauravpandey495@gmail.com <sauravRAA2123>
2021-03-28 12:46:49 +0530

    checkparity of a number in python

---
+++ numbers/CheckParityofaNumber.py
@@ -0,0 +1,11 @@
+def checkParity(n):
+  result = (n % 2)
+  return result
+  
+print("Odd parity", checkParity(17))
+print("Even parity", checkParity(16))
+
+
+#output
+#Odd parity 1
+#Even parity 0
